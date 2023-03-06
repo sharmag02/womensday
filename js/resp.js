b5e4973045955da13034b412323256cb52466eb0
Gaurav Kumar Sharma <91082465+sharmag02official@users.noreply.github.com>
2023-03-06 08:40:49 +0530

    Add files via upload

---
+++ js/resp.js
@@ -0,0 +1,72 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+
+
+
+
+function readMore(city) {
+    let dots = document.querySelector(`.card[data-city="${city}"] .dots`);
+    let moreText = document.querySelector(`.card[data-city="${city}"] .more`); 
+    let btnText = document.querySelector(`.card[data-city="${city}"] .myBtn`);
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.textContent = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.textContent = "Read less"; 
+        moreText.style.display = "inline";
+    }
+}
+
+
+var slideshowContainers = document.getElementsByClassName("slideshow-container");
+/* For each container get starting variables */
+for(let s = 0; s < slideshowContainers.length; s++) {
+    /* Read the new data attribute */        
+    var cycle = slideshowContainers[s].dataset.cycle;
+    /* Find all the child nodes with class mySlides */
+    var slides = slideshowContainers[s].querySelectorAll('.mySlides');
+    var slideIndex = 0;
+    /* Now we can cycle slides, but this recursive function must have parameters */
+    /* slides and cycle never change, those are unique for each slide container */
+    /* slideIndex will increase during each iteration */
+    showSlides(slides, slideIndex, cycle);
+};
+
+/* Function is alsmost same, but now it uses 3 new parameters */
+function showSlides(slides, slideIndex, cycle) {
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    };
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    };
+    slides[slideIndex - 1].style.display = "block";
+    /* Calling same function, but with new parameters and cycle time */
+    setTimeout(function() {
+        showSlides(slides, slideIndex, cycle)
+    }, cycle);
+};
+
+
+
+
+
+
+
+
+
+
+
+
