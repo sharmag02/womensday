ebfdafa41164ea902e440d9a7185fdefb7b9732f
Gaurav Kumar Sharma <91082465+sharmag02official@users.noreply.github.com>
2023-03-07 18:14:58 +0530

    Add files via upload

--- js/resp.js
+++ js/resp.js
@@ -90,19 +90,7 @@ document.querySelector(".dropbtns").addEventListener("click", function() {
   });
  
 
-  var typed = new Typed(".typing", {
-    strings: ["Mother", "Sister", "Wife", "Daughter"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-
-var typed = new Typed(".typing-2", {
-    strings: ["Mother", "Sister", "Wife", "Daughter"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
+ 
 
   
   
---
+++ js/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+    strings: ["Mother", "Sister", "Wife", "Daughter"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Mother", "Sister", "Wife", "Daughter"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
