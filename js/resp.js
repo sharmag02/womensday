cea70b4e3aa7b4facceaa4cb5825475590cfc2c5
Gaurav Kumar Sharma <91082465+sharmag02official@users.noreply.github.com>
2023-03-07 18:09:00 +0530

    Add files via upload

--- js/resp.js
+++ js/resp.js
@@ -89,9 +89,28 @@ document.querySelector(".dropbtns").addEventListener("click", function() {
     }
   });
  
+
+  var typed = new Typed(".typing", {
+    strings: ["Mother", "Sister", "Wife", "Daughter"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Mother", "Sister", "Wife", "Daughter"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
   
   
   
+
+
+
+
   
   
   
